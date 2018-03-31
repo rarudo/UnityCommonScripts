a789fac7f3e7c936abb6a4252ced61b876aa6e8b
rarudo <rarudonet@gmail.com>
2018-03-31 19:52:58 +0900

    Fix: Touchスクリプト形式のクリック判定を再追加

--- WooWhaController.cs
+++ WooWhaController.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TouchScript.Gestures;
 using UnityEngine;
 
+[RequireComponent(typeof(TapGesture))]
 public class WooWhaController :MonoBehaviour, ITouchable{
 
     //この距離以内だったらタップして反応する
@@ -13,9 +15,9 @@ public class WooWhaController :MonoBehaviour, ITouchable{
 		
     }
 	
- //   void OnEnable(){
- //       GetComponent<TapGesture>().Tapped += tappedHandle;
- //   }
+    void OnEnable(){
+        GetComponent<TapGesture>().Tapped += tappedHandle;
+    }
 	
     void tappedHandle(object sender, System.EventArgs e)
     {
