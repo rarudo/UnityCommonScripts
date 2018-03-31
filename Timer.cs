feaa0b9e18f14d452cad0e1b604c6b82c4af7b35
rarudo <rarudonet@gmail.com>
2018-03-31 20:52:35 +0900

    Feat: タイマー機能を追加

---
+++ Timer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+	[SerializeField] private int _countFrom;
+	private int _count;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_count = _countFrom;
+		StartCoroutine(LoopPerSecond());
+	}
+
+	IEnumerator LoopPerSecond()
+	{
+		while (true)
+		{
+			_count--;
+			yield return new WaitForSecondsRealtime(1);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
