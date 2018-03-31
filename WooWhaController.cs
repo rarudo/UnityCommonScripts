20ead46dcf745d643ec5256f34533ca63708e083
rarudo <rarudonet@gmail.com>
2018-03-31 16:58:51 +0900

    Feat; WooとWahをコントロールするためのクラスを実装

---
+++ WooWhaController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TouchScript.Gestures;
+using UnityEngine;
+
+[RequireComponent(typeof(TapGesture))]
+public class WooWhaController :MonoBehaviour, ITouchable{
+
+    //この距離以内だったらタップして反応する
+    [SerializeField]
+    private float _touchableLengh;
+
+    // Use this for initialization
+    void Start () {
+		
+    }
+	
+    void OnEnable(){
+        GetComponent<TapGesture>().Tapped += tappedHandle;
+    }
+	
+    void tappedHandle(object sender, System.EventArgs e)
+    {
+        if (IsTouchableRange())
+        {
+            Touch();
+        }
+        else
+        {
+            print("遠いからタップ判定無し");
+        }
+    }
+	
+    // Update is called once per frame
+    void Update () {
+		
+    }
+
+    public bool IsTouchableRange()
+    {
+        if (Vector3.Distance(Camera.main.transform.position, transform.position) < _touchableLengh)
+            return true;
+        return false;
+    }
+
+    public void Touch()
+    {
+        print("見つかっちゃったby"+transform.name);
+        Destroy(gameObject);
+    }
+}
