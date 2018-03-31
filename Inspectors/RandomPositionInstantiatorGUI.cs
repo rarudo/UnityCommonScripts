b2fd05f99e488de84eb7cad6172dd7426a03eb68
rarudo <rarudonet@gmail.com>
2018-03-31 10:50:22 +0900

    Feat: ランダムにオブジェクトを配置するメソッドを追加

---
+++ Inspectors/RandomPositionInstantiatorGUI.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(RandomPositionInstantiator))]
+public class RandomPositionInstantiatorGUI : MonoBehaviour {
+	[SerializeField] private GameObject _obj;
+	[SerializeField] private Vector3 _posStart;
+	[SerializeField] private Vector3 _posEnd;
+	// Use this for initialization
+	void Start()
+	{
+		var rpi = GetComponent<RandomPositionInstantiator>();
+		rpi.InstantiateObj(_obj,_posStart,_posEnd);
+	}
+
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ RandomPositionInstantiator.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomPositionInstantiator : MonoBehaviour {
+
+
+	public void InstantiateObj(GameObject prefab, Vector3 posStart, Vector3 posEnd)
+	{
+        float x = Random.Range(posStart.x, posEnd.x);
+        float y = Random.Range(posStart.y, posEnd.y);
+        float z = Random.Range(posStart.z, posEnd.z);
+        GameObject obj = Instantiate(prefab, new Vector3(x, y, z), new Quaternion());
+        obj.SetActive(true);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
