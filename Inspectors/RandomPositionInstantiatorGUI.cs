b26441ce2303711f64ebde64fd19e8e53d5356a7
rarudo <rarudonet@gmail.com>
2018-03-31 11:07:32 +0900

    Fix: staticメソッドに変更

--- Inspectors/RandomPositionInstantiatorGUI.cs
+++ Inspectors/RandomPositionInstantiatorGUI.cs
@@ -1,22 +1,22 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-[RequireComponent(typeof(RandomPositionInstantiator))]
-public class RandomPositionInstantiatorGUI : MonoBehaviour {
-	[SerializeField] private GameObject _obj;
-	[SerializeField] private Vector3 _posStart;
-	[SerializeField] private Vector3 _posEnd;
-	// Use this for initialization
-	void Start()
-	{
-		var rpi = GetComponent<RandomPositionInstantiator>();
-		rpi.InstantiateObj(_obj,_posStart,_posEnd);
-	}
+namespace CommonScripts.Inspectors
+{
+	[RequireComponent(typeof(RandomPositionInstantiator))]
+	public class RandomPositionInstantiatorGUI : MonoBehaviour {
+		[SerializeField] private GameObject _obj;
+		[SerializeField] private Vector3 _posStart;
+		[SerializeField] private Vector3 _posEnd;
+		// Use this for initialization
+		void Start()
+		{
+			RandomPositionInstantiator.InstantiateObj(_obj,_posStart,_posEnd);
+		}
 
 	
-	// Update is called once per frame
-	void Update () {
+		// Update is called once per frame
+		void Update () {
 		
+		}
 	}
 }
--- RandomPositionInstantiator.cs
+++ RandomPositionInstantiator.cs
@@ -5,13 +5,24 @@ using UnityEngine;
 public class RandomPositionInstantiator : MonoBehaviour {
 
 
-	public void InstantiateObj(GameObject prefab, Vector3 posStart, Vector3 posEnd)
+	/// <summary>
+	/// 空間内にランダムにオブジェクトを置くメソッド
+	/// </summary>
+	/// <param name="prefab"></param>
+	/// <param name="posStart"></param>
+	/// <param name="posEnd"></param>
+	public static void InstantiateObj(GameObject prefab, Vector3 posStart, Vector3 posEnd)
 	{
         float x = Random.Range(posStart.x, posEnd.x);
         float y = Random.Range(posStart.y, posEnd.y);
         float z = Random.Range(posStart.z, posEnd.z);
         GameObject obj = Instantiate(prefab, new Vector3(x, y, z), new Quaternion());
         obj.SetActive(true);
+	}
+
+	public static void InstantiateObj(GameObject prefab, GameObject startObj, GameObject endObj)
+	{
+		InstantiateObj(prefab, startObj.transform.position, endObj.transform.position);
 	}
 	
 	// Update is called once per frame
